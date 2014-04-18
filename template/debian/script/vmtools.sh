4d29769fe90158f1eb466b47579ca1a4397f34d0
Will Farrell <will@mojotech.com>
2014-04-18 15:13:21 -0400

    Use patched vmware tools to handle 3.13 kernel.

--- template/debian/script/vmtools.sh
+++ template/debian/script/vmtools.sh
@@ -8,6 +8,7 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     mkdir -p /mnt/cdrom
     mount -o loop /home/vagrant/linux.iso /mnt/cdrom
     tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
+    mv /tmp/patched-vmhgfs.tar /tmp/vmware-tools-distrib/lib/modules/source/vmhgfs.tar
     /tmp/vmware-tools-distrib/vmware-install.pl -d
     rm /home/vagrant/linux.iso
     umount /mnt/cdrom
